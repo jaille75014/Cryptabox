6bac37eb68fbf1c059e87386b60bbd1f05938583
alban <alban.cabadet@gmail.com>
2025-01-27 23:01:27 +0100

    first stepppp

--- main.c
+++ main.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
-#include "src/security/encrypt.h"
-#include "src/security/decrypt.h"
+// #include "src/security/encrypt.h"
+// #include "src/security/decrypt.h"
+void printMenu(){
 
+    printf("\n1. Crypter un fichier avec l'aide d'un mot de passe.\n2. Décrypter un fichier avec l'aide d'un mot de passe.\n3. Quitter.");
+
+}
 
 int main() {
-    const char *data = "exemplededata123"; // 16 octets
-    const char *key = "0123456789abcdef"; // 16 octets (clé fixe)
-    char encrypted_data[16];
-    char decrypted_data[16];
+    char action=0;
 
+    printf("Bonjour ! Bienvenue dans votre outil préféré, CryptaBox.\n");
 
-    if (encrypt(data, key, encrypted_data) == 0) {
-        printf("Données chiffrées : ");
-        for (int i = 0; i < 16; i++) {
-            printf("%02x", (unsigned char)encrypted_data[i]);
-        }
-        printf("\n");
-    } else {
-        printf("Erreur lors du chiffrement !\n");
-    }
+    do {
 
-    if (decrypt(encrypted_data, key, decrypted_data) == 0) {
-        printf("Données déchiffrées : %s\n", decrypted_data);
-    } else {
-        printf("Erreur lors du déchiffrement !\n");
-    }
+        do{
+            
+            printMenu();
+
+            printf("\nVotre action : ");
+            scanf("%d",&action);
+
+            if(action<1||action>3){
+                fprintf(stderr,"Entrez une option valide.");
+            }
+
+        } while(action<1||action>3);
+
+    
+
+    } while(1);
 
 
     exit(EXIT_SUCCESS);
