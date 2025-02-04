4544e7f3f95ae083ad58181adbd28f5118b6c19d
RafaLe0 <fron.rafael@gmail.com>
2025-02-04 22:26:30 +0100

    modified:   src/user_management/users.c

--- src/user_management/users.c
+++ src/user_management/users.c
@@ -10,9 +10,9 @@
 #define MAX_USERNAME 50
 #define PASSWORD_MAX_LENGTH 256
 #define HASH_SIZE 65
-#define SERVER "localhost"
-#define USERNAME "AJR3MOUSQUETAIRES"
-#define PASSWORD "MonSuperMotDePasse"
+#define SERVER "192.168.1.36"
+#define USERNAME "ajr3mousquetaires"
+#define PASSWORD "AJR3MOUSQUETAIRES"
 
 user *currentUser = NULL;
 
